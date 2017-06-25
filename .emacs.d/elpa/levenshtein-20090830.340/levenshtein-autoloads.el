e789d9b73e46d75e1316a66f77fea21255058d74
Evgeny Panasyuk <evgeny.panasyuk@gmail.com>
2017-06-26 00:46:38 +0800

    added several packages

---
+++ .emacs.d/elpa/levenshtein-20090830.340/levenshtein-autoloads.el
@@ -0,0 +1,16 @@
+;;; levenshtein-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("levenshtein.el") (22863 55686 590746
+;;;;;;  581000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; levenshtein-autoloads.el ends here
