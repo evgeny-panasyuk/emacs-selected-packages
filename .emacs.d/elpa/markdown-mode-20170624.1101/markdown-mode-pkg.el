145b45f64251e3e261c40f3c71a20766a71fb82f
Evgeny Panasyuk <evgeny.panasyuk@gmail.com>
2017-06-26 00:48:15 +0800

    added several packages

---
+++ .emacs.d/elpa/markdown-mode-20170624.1101/markdown-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "markdown-mode" "20170624.1101" "Major mode for Markdown-formatted text" '((emacs "24") (cl-lib "0.5")) :commit "0e241436cf0b21a93f0f77591f8b709ac7ff4846" :url "http://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
