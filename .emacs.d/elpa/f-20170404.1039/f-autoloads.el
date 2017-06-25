3d8dce873585a68a0c2350ab2b2272b4f80e8bd0
Evgeny Panasyuk <evgeny.panasyuk@gmail.com>
2017-06-26 00:33:25 +0800

    added several packages

---
+++ .emacs.d/elpa/f-20170404.1039/f-autoloads.el
@@ -0,0 +1,15 @@
+;;; f-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("f.el") (22863 55632 174746 618000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; f-autoloads.el ends here
